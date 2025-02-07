986fd6d5be42b7f9fe45d176d6da038ef4ed27d7
gadhvirushiraj <gadhvirushiraj@gmail.com>
2025-02-07 19:17:55 +0530

    styling to guidlines

--- src/app/components/GuideLines.jsx
+++ src/app/components/GuideLines.jsx
@@ -5,33 +5,33 @@ export default function GuideLines({ frame, centerOffset }) {
   const borderWidth = 3
   const guideWidth = 2
 
-  const left = frame.x + centerOffset.x - borderWidth;
-  const top = frame.y + centerOffset.y - borderWidth;
-  const right = left + frame.width + 2*borderWidth + guideWidth;
-  const bottom = top + frame.height + 2*borderWidth + guideWidth;
+  const left = frame.x + centerOffset.x - borderWidth - guideWidth;
+  const top = frame.y + centerOffset.y - borderWidth - guideWidth;
+  const right = left + frame.width + 2*(borderWidth + guideWidth);
+  const bottom = top + frame.height + 2*(borderWidth + guideWidth);
 
   return (
     <>
       <div // Left line
-        className={`guide-line left-0 top-0 w-[${guideWidth}px] h-full`}
+        className="guide-line left-0 top-0 w-[2px] h-full"
         style={{
           left: left - 1,
         }}
       />
       <div // Right line
-        className={`guide-line left-0 top-0 w-[${guideWidth}px] h-full`}
+        className="guide-line left-0 top-0 w-[2px] h-full"
         style={{
           left: right - 1,
         }}
       />
       <div // Top line
-        className={`guide-line top-0 left-0 w-full h-[${guideWidth}px]`}
+        className="guide-line top-0 left-0 w-full h-[2px]"
         style={{
           top: top - 1,
         }}
       />
       <div // Bottom line
-        className={`guide-line top-0 left-0 w-full h-[${guideWidth}px]`}
+        className="guide-line top-0 left-0 w-full h-[2px]"
         style={{
           top: bottom - 1,
         }}
